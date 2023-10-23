a96a697e4d558697eb96b6dc6729b35faa6a1b69
EarthIsHeaven <thinleylama44@gmail.com>
2023-10-23 22:23:08 +0530

    Basic code edit in 01basicreact

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -2,7 +2,7 @@
 
 function App() {
   return (
-    <h1>chai aur react</h1>
+    <h1>chai aur react | Here, i edited the 01basicreact by deleting some file which i was not able to understand and updating code in app.js and index.js</h1>
   );
 }
 
