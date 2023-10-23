5fc180af39571c294bf31d40d7df8a6272e75e6c
EarthIsHeaven <thinleylama44@gmail.com>
2023-10-23 23:16:01 +0530

    Basic code cleaning in 01basicreact

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,8 +1,6 @@
-
-
 function App() {
   return (
-    <h1>chai aur react | Here, i edited the 01basicreact by deleting some file which i was not able to understand and updating code in app.js and index.js</h1>
+    <h1>chai aur react | Here, i have edited the 01basicreact by deleting some file which i was not able to understand and updated code in app.js and index.js</h1>
   );
 }
 
