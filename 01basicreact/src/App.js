5e431018f8011abb29543b967d9824fe4a771ba6
EarthIsHeaven <thinleylama44@gmail.com>
2023-10-24 10:27:19 +0530

    Implemented in basicreact and func name initial letter must be in capital letter.

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,6 +1,8 @@
+import Chai from "./chai"
+
 function App() {
   return (
-    <h1>chai aur react | Here, i have edited the 01basicreact by deleting some file which i was not able to understand and updated code in app.js and index.js</h1>
+    <Chai/>
   );
 }
 
