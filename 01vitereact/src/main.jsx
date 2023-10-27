520ad5849f77982a8185073c096494e45f825332
EarthIsHeaven <thinleylama44@gmail.com>
2023-10-27 17:31:13 +0530

    React component in main.jsx in vite

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -10,8 +10,20 @@ function MyApp(){
   )
 }
 
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit google
+  </a>
+)
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'click me to visit google'
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <MyApp />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <MyApp />
+  // </React.StrictMode>,
+  reactElement
 )
