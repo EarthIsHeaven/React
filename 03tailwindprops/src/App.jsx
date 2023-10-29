a5891492e8fb4e648448a0b499e05350b20a1163
EarthIsHeaven <thinleylama44@gmail.com>
2023-10-29 15:38:37 +0530

    Props used

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,15 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Card from './components/card'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
+  let myObj={
+    name: 'Don',
+    age: '11'
+  }
 
   return (
     <>
       <h1 className='bg-green-500 p-4 rounded-xl'>Tailwind</h1>
-      <Card />
+      <Card channel="Thinley" someObj={myObj}/>
       <Card />
     </>
   )
