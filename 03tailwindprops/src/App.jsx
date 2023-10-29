6b6b73d89f515fe08e892181af71e4f2ae7ee67b
EarthIsHeaven <thinleylama44@gmail.com>
2023-10-29 15:10:21 +0530

    React components/card with tailwind.

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/card'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-green-500'>Tailwind</h1>
+      <h1 className='bg-green-500 p-4 rounded-xl'>Tailwind</h1>
+      <Card />
+      <Card />
     </>
   )
 }
