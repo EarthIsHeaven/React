63892a832bc688b5d7dfbcc57a8854d4a82cd8c4
EarthIsHeaven <thinleylama44@gmail.com>
2023-10-30 18:58:16 +0530

    Styling done for buttons

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -15,10 +15,12 @@ function App() {
     style={{backgroundColor: color}}
     >
     <div className="fixed flex flex-wrap
-    justify-center bottom-12 insert-x-0 px-2">
+    justify-center bottom-12 inset-x-0">
       <div className="flex flex-wrap justify-center
-      gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl ">
-        button
+      gap-3 shadow-lg bg-white rounded-3xl py-3 px-10">
+        <button className="p-3 rounded-2xl " style={{backgroundColor: "red"}}>Red</button>
+        <button className="p-3 rounded-2xl " style={{backgroundColor: "green"}}>Green</button>
+        <button className="p-3 rounded-2xl " style={{backgroundColor: "blue"}}>Blue</button>
       </div>
     </div>
     </div>
