88bdd93384afc9d6d9384393b63cf56381d8ee35
EarthIsHeaven <thinleylama44@gmail.com>
2023-10-30 21:14:39 +0530

    bgcolor changer done.

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -3,13 +3,6 @@ import { useState } from 'react'
 function App() {
   const [color, setColor] = useState("olive")
 
-  const changeColor=()=>{
-    console.log("1");
-    color= "red";
-
-    setCounter(color);
-    }
-
   return (
     <div className="w-full h-screen duration-200"
     style={{backgroundColor: color}}
@@ -18,9 +11,9 @@ function App() {
     justify-center bottom-12 inset-x-0">
       <div className="flex flex-wrap justify-center
       gap-3 shadow-lg bg-white rounded-3xl py-3 px-10">
-        <button className="p-3 rounded-2xl " style={{backgroundColor: "red"}}>Red</button>
-        <button className="p-3 rounded-2xl " style={{backgroundColor: "green"}}>Green</button>
-        <button className="p-3 rounded-2xl " style={{backgroundColor: "blue"}}>Blue</button>
+        <button onClick={()=> setColor("red")} className="p-3 rounded-2xl " style={{backgroundColor: "red"}}>Red</button>
+        <button onClick={()=> setColor("green")} className="p-3 rounded-2xl " style={{backgroundColor: "green"}}>Green</button>
+        <button onClick={()=> setColor("blue")} className="p-3 rounded-2xl " style={{backgroundColor: "blue"}}>Blue</button>
       </div>
     </div>
     </div>
