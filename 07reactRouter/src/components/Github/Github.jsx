9843118509145be24428ed7b1ddefee774b6a926
EarthIsHeaven <thinleylama44@gmail.com>
2023-11-05 09:55:56 +0530

    Added mine profile and followers using api in Github.jsx

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import { Link } from 'react-router-dom'
 
 function Github(){
+    const [data, setData] = useState([])
+    useEffect(()=> {
+        fetch('https://api.github.com/users/EarthisHeaven')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            setData(data)
+        })
+    }, [])
+
     return (
-        <div className='bg-green-600 text-white text-3xl p-5 m-5'>Github followes: </div>
+        <div className='bg-gray-600 text-white text-3xl p-5 m-5'>
+        <div>Github followes: {data.followers}</div>
+        <Link className="flex items-center justify-center mt-11 mb-11">
+                        <img
+                            src={data.avatar_url}
+                            alt="profile"
+                            className= 'rounded-full w-64'
+                        />
+                    </Link>
+        </div>
     )
 }
 
