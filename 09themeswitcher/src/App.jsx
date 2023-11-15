b857087618ff1d4d5d7c518906a3ad2fe9d39692
EarthIsHeaven <thinleylama44@gmail.com>
2023-11-15 15:38:13 +0530

    Comment added.

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -23,7 +23,8 @@ function App() {
 
 
   return (
-    <ThemeProvider value={{themeMode, lightTheme, darkTheme}} >
+    // Anything inside this context Provider will get access to the context value.
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}} >  
             <div className="flex flex-wrap min-h-screen items-center">
                 <div className="w-full">
                     <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
