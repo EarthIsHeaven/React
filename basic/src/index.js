02896a6efac1d8140468bffbceba7dc23b702e5e
EarthIsHeaven <thinleylama44@gmail.com>
2023-12-13 19:03:28 +0530

    Basic of component.

--- basic/src/index.js
+++ basic/src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// function Greeting(){
-//     return <h2>Hello World!</h2>
-// }
+function Greeting(){
+    return (
+        <div>
+            <Person/>
+            <Address/>
+        </div>
+    )
+}
+
+const Person = ()=>{
+    return (
+    <h2>Thinley here!</h2>
+    )
+}
+const Address = ()=> <p>Kathmandu, Nepal.</p>
 
 // function Greeting(){
 //     return React.createElement('h1', {}, 'hello World');
 // }
 
-function Greeting(){
-    return React.createElement('div', {}, React.createElement('h1', {}, 'Hi'));
-}
+// function Greeting(){
+//     return React.createElement('div', {}, React.createElement('h1', {}, 'Hi'));
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
