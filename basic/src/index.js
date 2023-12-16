ab6f06b5baca9c9fcb2a5cac487040592bd929bc
EarthIsHeaven <thinleylama44@gmail.com>
2023-12-16 23:26:19 +0530

    CSS on react

--- basic/src/index.js
+++ basic/src/index.js
@@ -1,22 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function Greeting(){
-    return (
-        <div>
-            <Person/>
-            <Address/>
-        </div>
-    )
+import "./index.css";
+
+const BookList = () => {
+    return <section className='bookList'>
+        <Book />
+        <Book />
+        <Book />
+    </section>
 }
 
-
-const Person = ()=>{
-    return (
-    <h2>Thinley here!</h2>
-    )
+const Book = () => {
+    return <article className='book'>
+        <Image />
+        <Title />
+        <Author />
+    </article>
 }
-const Address = ()=> <p>Kathmandu, Nepal.</p>
+
+const Image = () => <img src='https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/71yHTLnNKSL._SY466_.jpg' alt='Breakout Trading Made Easy'/>
+const Title = () => <h2>Breakout Trading Made Easy</h2>
+const Author = () => {
+    return <h4>Sunil Gurjar</h4>
+}
+
+//Different ways to create component
+
+// const Person = ()=>{
+//     return (
+//     <h2>Thinley here!</h2>
+//     )
+// }
+// const Address = ()=> <p>Kathmandu, Nepal.</p>
 
 // function Greeting(){
 //     return React.createElement('h1', {}, 'hello World');
@@ -28,4 +44,4 @@ const Address = ()=> <p>Kathmandu, Nepal.</p>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<Greeting />);
+root.render(<BookList />);
