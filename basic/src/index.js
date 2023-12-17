36fe773807484feebc1868a4bce400b730e142b5
EarthIsHeaven <thinleylama44@gmail.com>
2023-12-17 17:03:46 +0530

    props done

--- basic/src/index.js
+++ basic/src/index.js
@@ -3,27 +3,36 @@ import ReactDOM from 'react-dom/client';
 
 import "./index.css";
 
-const title = 'Breakout Trading Made Easy';
-const author = 'Sunil Gurjar';
-const img = 'https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/71yHTLnNKSL._SY466_.jpg';
+const firstBook = {
+    title : 'Interesting Facts For Curious Minds: 1572',
+    author : 'Jordan Moore',
+    img : './images/book.jpg'
+}
+const secondBook = {
+    title : 'Oath and Honor: A Memoir and a Warning',
+    author : 'Liz Cheney',
+    img : './images/img2.jpg'
+}
+const thirdBook = {
+    title : 'The Heaven & Earth Grocery Store: A Novel',
+    author : 'James McBride',
+    img : './images/img3.jpg'
+}
 
 const BookList = () => {
     return <section className='bookList'>
-        <Book job='developer'/>
-        <Book title='random title' number={22}/>
-        <Book />
+        <Book title = {firstBook.title} author = {firstBook.author} img = {firstBook.img}/>
+        <Book title = {secondBook.title} author = {secondBook.author} img = {secondBook.img}/>
+        <Book title = {thirdBook.title} author = {thirdBook.author} img = {thirdBook.img}/>
     </section>
 }
 
 const Book = (props) => {
 
     return <article className='book'>
-        <img src={img} alt={title}/>
-        <h2>{title}</h2>
-        <h4>{author}</h4>
-        <p>{props.job}</p>
-        <p>{props.title}</p>
-        <p>{props.number}</p>
+        <img src={props.img} alt={props.title}/>
+        <h2>{props.title}</h2>
+        <h4>{props.author}</h4>
     </article>
 }
 
