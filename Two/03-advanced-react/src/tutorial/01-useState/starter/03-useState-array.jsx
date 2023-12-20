5498534431d9be4a896c769f5f75c8f43a23b1d7
EarthIsHeaven <thinleylama44@gmail.com>
2023-12-20 17:42:52 +0530

    03-useState done.

--- Two/03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ Two/03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,32 @@
+import { data } from "../../../data";
+import { useState } from "react";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  let [value, setValue] = useState(data);
+
+  const remove = (id) => {
+    let eliminate = value.filter((single)=> single.id !== id);
+    setValue(eliminate);
+  }
+
+  return(
+    <div>
+      {
+        value.map((person)=> {
+          const {id, name} = person;
+          return(
+            <div key={id}>
+              <h2>{name}</h2>
+              <button onClick={() => remove(id)}>remove</button>
+            </div>
+          )
+        })
+      }
+      <div>
+        <button onClick={()=>setValue([])}>Clear All</button>
+      </div>
+    </div>
+  )
 };
 
 export default UseStateArray;
